bde8a41675d40af03e85d339b0b8dc7a02d9b803
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 17:54:41 -0500

    Integrated Google API

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
+import time
+
 def get_data():
-    print("Reading in sequences.fasta")
+    print('Reading in sequences.fasta')
+    start_time=time.time()
     data = pd.read_csv('sequences.fasta',header=None,sep="|",lineterminator=">",verbose=True) #returns Dataframe
     data.columns = ['Name','Published Date','Length','Genome']
     for i in range(data.shape[1]):
         genome = data['Genome'][i]
         data.loc[:,('Genome',i)]=genome.replace('\n','')
+    print('Total time:',time.time()-start_time)
     return data
+
 if __name__ == '__main__':
     data = get_data()
     print(data)
